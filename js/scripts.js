84be49afebb2fd6b13e2bef177484f70a2ce5c08
Burgessdude <143647882+Burgessdude@users.noreply.github.com>
2024-10-30 23:09:33 -0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+const navElement = document.querySelector('#theNav');
+const btnElement = document.querySelector('#theButton');
+
+btnElement.addEventListener('click', () => {
+    btnElement.classList.toggle('open');
+    navElement.classList.toggle('open');
+});
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    effect: 'cube',
+    speed: 2000,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    // start autoplay
+    autoplay: {
+        delay: 5000,
+    },
+  });
